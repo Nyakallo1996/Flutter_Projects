289e85a11ab56a3059a2f2bccfb406245042fb7f
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2026-06-22 20:19:56 +0200

    creating separate files

---
+++ dice_app/lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 83, 18, 196),
+            Color.fromARGB(255, 39, 11, 87),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomLeft,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world!',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
+}
--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -1,37 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(),
-        ),
-    ),
-  );
-}
+import 'package:dice_app/gradient_container.dart';
 
-class GradientContainer extends StatelessWidget{
-  const GradientContainer({super.key});
-  
-  
-  @override
-  Widget build(context) {
-    return Container(
-        decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [  Color.fromARGB(255, 83, 18, 196),
-           Color.fromARGB(255, 39, 11, 87) ],
-           begin: Alignment.topLeft,
-           end: Alignment.bottomLeft,
-        ),
-      ), 
-      child: const Center(
-        child: Text('Hello world!', style: TextStyle(
-          color: Colors.white,
-          fontSize: 28,
-        ),
-        ),
-        ),
-        );
-  }
+void main() {
+  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
 }

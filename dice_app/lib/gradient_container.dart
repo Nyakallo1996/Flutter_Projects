38e337970777c8b2d02c8e36f549f2c5e8693743
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2026-06-22 21:38:56 +0200

    introducing variables

--- dice_app/lib/gradient_container.dart
+++ dice_app/lib/gradient_container.dart
@@ -1,20 +1,23 @@
 import 'package:dice_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment =  Alignment.bottomLeft;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration:  BoxDecoration(
         gradient: LinearGradient(
           colors: [
             Color.fromARGB(255, 83, 18, 196),
             Color.fromARGB(255, 39, 11, 87),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomLeft,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(

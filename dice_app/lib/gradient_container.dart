62c47b2936b17612c67492dc96cee3d3c5b2998d
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2026-07-01 21:57:40 +0200

    small changes

--- dice_app/lib/gradient_container.dart
+++ dice_app/lib/gradient_container.dart
@@ -29,9 +29,18 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset('assets/images/dice-2.png', width: 200),
-            TextButton(onPressed: rollDice, child: Text('Roll Dice')),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28),
+              ),
+              child: Text('Roll Dice'),
+            ),
           ],
         ),
       ),
